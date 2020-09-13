a742913bed6cbe2480edfdea8dea89636696c38a
LionelKarlen <lionel.karlen@protonmail.com>
2020-09-13 18:24:49 +0200

    Implemented server setup

--- Server.dart
+++ Server.dart
@@ -1,20 +1,26 @@
+import 'dart:convert';
 import 'dart:io';
 import 'Cryptocurrency.dart';
 import 'Trader.dart';
 
 Future main() async {
-  var server = await HttpServer.bind(
-    InternetAddress.loopbackIPv4,
-    4040,
-  );
-  print('Listening on localhost:${server.port}');
+  	var server = await HttpServer.bind(
+		InternetAddress.loopbackIPv4,
+		4040,
+  	);
+  	print('Listening on localhost:${server.port}');
 
 	Cryptocurrency cryptocurrency = new Cryptocurrency("bitcoin","btc","Bitcoin");
 	LevelUpTrader trader = LevelUpTrader(cryptocurrency,10,.01,.5);
 
-  await for (HttpRequest request in server) {
-	print("Request");
-    request.response.write(trader.funds.toString()+"\n"+trader.heldFunds.toString());
-    await request.response.close();
-  }
+  	await for (HttpRequest request in server) {
+		Object responseObject = {
+			"cryptocurrency": trader.cryptocurrency.symbol,
+			"investedfunds": trader.funds,
+			"heldFunds":trader.heldFunds,
+		};
+		dynamic json = jsonEncode(responseObject);
+		request.response.write(json);
+		await request.response.close();
+  	}
 }
