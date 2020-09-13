64fa9d55a0dd3437b7eecf4872e17c5f483d0fef
LionelKarlen <lionel.karlen@protonmail.com>
2020-09-13 18:35:59 +0200

    Fixed server setup

--- bin/Debug.dart
+++ bin/Debug.dart
@@ -12,6 +12,6 @@ main(List<String> args) {
 Cryptocurrency cryptocurrency = new Cryptocurrency("bitcoin","btc","Bitcoin");
 CoinGeckoImporter i = new CoinGeckoImporter(cryptocurrency, Currency.CHF);
 // print(i.getMarketChart(2).then((list) {print(list);}));
-	// Trader trader = LevelUpBacktestTrader(cryptocurrency,10,.02,.25,File("./data/btc-14d-10-9-20.json"));
+	// Trader trader = LevelUpBacktestTrader(cryptocurrency,10,.02,.25,File("../data/btc-14d-10-9-20.json"));
 Trader trader = LevelUpTrader(cryptocurrency,10,.01,.5);
 }
